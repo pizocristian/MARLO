3eb7e766298fdcaf4f783ffd85f7c5bb5066e96b
jurodca <jrodriguezc@gmail.com>
2018-04-24 11:22:13 -0500

    - Fix issue with migration of the procedure

--- marlo-web/src/main/resources/database/migrations/V2_5_0_20180423_1039__BIPersmissionsProcedure.sql
+++ marlo-web/src/main/resources/database/migrations/V2_5_0_20180423_1039__BIPersmissionsProcedure.sql
@@ -1,11 +1,19 @@
-DROP PROCEDURE IF EXISTS marlodb.assignBIPermissions;
-CREATE PROCEDURE marlodb.`assignBIPermissions` (IN `userID` bigint,type TINYINT(1),urlBI text, userBI text, passwordBI text)
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Procedure structure for assignBIPermissions
+-- ----------------------------
+DROP PROCEDURE
+IF EXISTS marlodb.`assignBIPermissions`;
+DELIMITER ;;
+
+CREATE PROCEDURE marlodb.`assignBIPermissions` (IN `userID` bigint,`typeBi` TINYINT(1),`urlBI` varchar(500), `userBI` varchar(500), `passwordBI` varchar(500))
 BEGIN
 
-IF (select COUNT('x') from bi_permissions p  where p.userID=`userID` and p.type = type) =0 THEN
-    
-    insert into bi_permissions (type,user,urlbi,userBi, userPass,is_active) values (type,`userID`,urlBI,userBI,passwordBI,1);
-
+if (select COUNT('x') from bi_permissions p  where p.user=`userID` and p.type = `typeBi`) =0 THEN
+    insert into bi_permissions (`type`,`user`,`urlbi`,`userBi`, `userPass`,`is_active`) values (`typeBi`,`userID`,`urlBI`,`userBI`,`passwordBI`,1);
 END IF;
 
-END;
+END;;
+DELIMITER;
+

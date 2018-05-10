05d9d33f8c05ff9ff0933b8d3d9a27312ebc6c42
jurodca <jrodriguezc@gmail.com>
2018-05-10 14:29:20 -0500

    - add permissions in user_roles table

--- marlo-web/src/main/resources/database/migrations/V2_5_0_20180423_1039__BIPersmissionsProcedure.sql
+++ marlo-web/src/main/resources/database/migrations/V2_5_0_20180423_1039__BIPersmissionsProcedure.sql
@@ -7,12 +7,35 @@ DROP PROCEDURE
 IF EXISTS marlodb.`assignBIPermissions`;
 DELIMITER ;;
 
-CREATE PROCEDURE marlodb.`assignBIPermissions` (IN `userID` bigint,`typeBi` TINYINT(1),`urlBI` varchar(500), `userBI` varchar(500), `passwordBI` varchar(500))
+CREATE PROCEDURE marlodb.`assignBIPermissions` (IN `userID` bigint, IN `globalId` bigint,`typeBi` TINYINT(1),`urlBI` varchar(500), `userBI` varchar(500), `passwordBI` varchar(500))
 BEGIN
 
-if (select COUNT('x') from bi_permissions p  where p.user=`userID` and p.type = `typeBi`) =0 THEN
-    insert into bi_permissions (`type`,`user`,`urlbi`,`userBi`, `userPass`,`is_active`) values (`typeBi`,`userID`,`urlBI`,`userBI`,`passwordBI`,1);
-END IF;
+    if (select COUNT('x') from bi_permissions p  where p.user=`userID` and p.type = `typeBi`) =0 THEN
+        insert into bi_permissions (`type`,`user`,`urlbi`,`userBi`, `userPass`,`is_active`) values (`typeBi`,`userID`,`urlBI`,`userBI`,`passwordBI`,1);
+    END IF;
+
+    IF (typeBi = 1) THEN
+
+      IF(select count(1) from user_roles
+         where user_id = `userID`
+         and role_id = (select id from roles where roles.acronym = 'BID' and roles.global_unit_id= `globalId`)) = 0 THEN
+         
+         insert into user_roles (user_id,role_id)values(`userID`,(select id from roles where roles.acronym = 'BID' and roles.global_unit_id= `globalId`)); 
+  
+      END IF;
+  
+    END IF;
+    
+    IF (typeBi = 2) THEN
+      IF(select count(1) from user_roles
+         where user_id = `userID`
+         and role_id = (select id from roles where roles.acronym = 'BIA' and roles.global_unit_id= `globalId`)) = 0 THEN
+         
+         insert into user_roles (user_id,role_id)values(`userID`,(select id from roles where roles.acronym = 'BID' and roles.global_unit_id= `globalId`));
+     
+      END IF; 
+
+    END IF;
 
 END;;
 DELIMITER;

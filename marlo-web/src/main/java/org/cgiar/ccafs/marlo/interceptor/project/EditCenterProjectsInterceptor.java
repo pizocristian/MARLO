5daaab8ec3699d04183fcfd5c03eb3753e000c54
Diego Fernando Perez <d.f.perez@cgiar.org>
2019-09-27 11:45:08 -0500

    Advance project Marlo Centers #1965 Restric access to not submitted projects add non dependency from globalunit project rules for project list

---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/interceptor/project/EditCenterProjectsInterceptor.java
@@ -0,0 +1,402 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+package org.cgiar.ccafs.marlo.interceptor.project;
+
+import org.cgiar.ccafs.marlo.action.BaseAction;
+import org.cgiar.ccafs.marlo.config.APConstants;
+import org.cgiar.ccafs.marlo.data.manager.GlobalUnitManager;
+import org.cgiar.ccafs.marlo.data.manager.GlobalUnitProjectManager;
+import org.cgiar.ccafs.marlo.data.manager.LiaisonUserManager;
+import org.cgiar.ccafs.marlo.data.manager.PhaseManager;
+import org.cgiar.ccafs.marlo.data.manager.ProjectManager;
+import org.cgiar.ccafs.marlo.data.model.GlobalUnit;
+import org.cgiar.ccafs.marlo.data.model.GlobalUnitProject;
+import org.cgiar.ccafs.marlo.data.model.Institution;
+import org.cgiar.ccafs.marlo.data.model.LiaisonInstitution;
+import org.cgiar.ccafs.marlo.data.model.LiaisonUser;
+import org.cgiar.ccafs.marlo.data.model.Phase;
+import org.cgiar.ccafs.marlo.data.model.Project;
+import org.cgiar.ccafs.marlo.data.model.ProjectPartner;
+import org.cgiar.ccafs.marlo.data.model.ProjectSectionStatusEnum;
+import org.cgiar.ccafs.marlo.data.model.ProjectStatusEnum;
+import org.cgiar.ccafs.marlo.data.model.SharedProjectSectionStatusEnum;
+import org.cgiar.ccafs.marlo.data.model.User;
+import org.cgiar.ccafs.marlo.security.Permission;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+import org.apache.struts2.dispatcher.Parameter;
+
+
+public class EditCenterProjectsInterceptor extends AbstractInterceptor implements Serializable {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 5712697573159769112L;
+  private Map<String, Parameter> parameters;
+  private Map<String, Object> session;
+  private GlobalUnit crp;
+  private long projectId = 0;
+  private GlobalUnit loggedCrp;
+  private Phase phase;
+  private final GlobalUnitManager crpManager;
+  private final ProjectManager projectManager;
+  private final PhaseManager phaseManager;
+  private final GlobalUnitProjectManager globalUnitProjectManager;
+  private final LiaisonUserManager liaisonUserManager;
+
+  @Inject
+  public EditCenterProjectsInterceptor(ProjectManager projectManager, GlobalUnitManager crpManager,
+    PhaseManager phaseManager, GlobalUnitProjectManager globalUnitProjectManager,
+    LiaisonUserManager liaisonUserManager) {
+    this.projectManager = projectManager;
+    this.crpManager = crpManager;
+    this.phaseManager = phaseManager;
+    this.globalUnitProjectManager = globalUnitProjectManager;
+    this.liaisonUserManager = liaisonUserManager;
+  }
+
+  @Override
+  public String intercept(ActionInvocation invocation) throws Exception {
+    parameters = invocation.getInvocationContext().getParameters();
+    session = invocation.getInvocationContext().getSession();
+    crp = (GlobalUnit) session.get(APConstants.SESSION_CRP);
+    try {
+      this.setPermissionParameters(invocation);
+      return invocation.invoke();
+    } catch (Exception e) {
+      e.printStackTrace();
+      return BaseAction.NOT_FOUND;
+    }
+  }
+
+  void setPermissionParameters(ActionInvocation invocation) throws Exception {
+    BaseAction baseAction = (BaseAction) invocation.getAction();
+    baseAction.setBasePermission(null);
+    baseAction.setSession(session);
+    loggedCrp = (GlobalUnit) session.get(APConstants.SESSION_CRP);
+    loggedCrp = crpManager.getGlobalUnitById(loggedCrp.getId());
+
+    User user = (User) session.get(APConstants.SESSION_USER);
+    baseAction.setSession(session);
+    phase = baseAction.getActualPhase();
+    phase = phaseManager.getPhaseById(phase.getId());
+
+    boolean canEdit = false;
+    boolean hasPermissionToEdit = false;
+    boolean editParameter = false;
+    boolean canSwitchProject = false;
+    boolean isAdmin = false;
+
+    boolean contactPointEditProject = baseAction.hasSpecificities(APConstants.CRP_CONTACT_POINT_EDIT_PROJECT);
+
+    String projectParameter = parameters.get(APConstants.PROJECT_REQUEST_ID).getMultipleValues()[0];
+
+    projectId = Long.parseLong(projectParameter);
+
+    Project project = projectManager.getProjectById(projectId);
+    // globalunit center
+    if (loggedCrp.isCenterType()) {
+      if (project != null && project.isActive()) {
+        Phase projectPhase = project.getCurrentPhase();
+        if (projectPhase != null) {
+          if (!phase.getCrp().getId().equals(loggedCrp.getId())) {
+            GlobalUnitProject globalUnitProject =
+              globalUnitProjectManager.findByProjectAndGlobalUnitId(project.getId(), projectPhase.getCrp().getId());
+            if (globalUnitProject != null) {
+              if (project != null && project.isActive()) {
+                if (!globalUnitProject.isOrigin()) {
+                  GlobalUnitProject globalUnitProjectOrigin = globalUnitProjectManager.findByProjectId(project.getId());
+                  List<Phase> phases = globalUnitProjectOrigin.getGlobalUnit().getPhases().stream()
+                    .filter(c -> c.isActive() && c.getDescription().equals(baseAction.getActualPhase().getDescription())
+                      && c.getYear() == baseAction.getActualPhase().getYear() && c.getUpkeep())
+                    .collect(Collectors.toList());
+                  if (phases.size() > 0) {
+                    baseAction.setPhaseID(phases.get(0).getId());
+                    project.getProjecInfoPhase(phases.get(0));
+                  }
+                }
+                String params[] = {crp.getAcronym(), project.getId() + "",
+                  baseAction.getActionName().replaceAll(crp.getAcronym() + "/", "")};
+                if (baseAction.canAccessSuperAdmin() || baseAction.canEditCrpAdmin()) {
+                  isAdmin = true;
+                  if (!baseAction.isSubmit(projectId)) {
+                    canSwitchProject = true;
+                  }
+                  canEdit = true;
+                } else {
+                  if (baseAction.hasPermission(baseAction.generatePermission(Permission.PROJECT__PERMISSION, params))) {
+                    canEdit = true;
+                  }
+                  if (project.getProjecInfoPhase(baseAction.getActualPhase()) != null) {
+
+                    if (!project.getProjecInfoPhase(baseAction.getActualPhase()).isProjectEditLeader()
+                      && !baseAction.hasPermission(baseAction.generatePermission(Permission.PROJECT__SWITCH, params))) {
+                      canEdit = false;
+                    }
+                  }
+
+                  LiaisonUser lUser =
+                    liaisonUserManager.getLiaisonUserByUserId(user.getId(), projectPhase.getCrp().getId());
+                  if (contactPointEditProject && lUser != null) {
+                    LiaisonInstitution liaisonInstitution = lUser.getLiaisonInstitution();
+                    ProjectPartner projectPartner = project.getLeader();
+
+                    if (projectPartner != null) {
+                      Institution institutionProject = projectPartner.getInstitution();
+
+                      Institution institutionCp = liaisonInstitution.getInstitution();
+
+                      if (institutionCp != null) {
+                        if (institutionCp.getId().equals(institutionProject.getId())) {
+                          canSwitchProject = true;
+                        } else {
+                          if (baseAction
+                            .hasPermission(baseAction.generatePermission(Permission.PROJECT__SWITCH, params))) {
+                            canSwitchProject = true;
+                          }
+                        }
+                      } else {
+                        if (baseAction
+                          .hasPermission(baseAction.generatePermission(Permission.PROJECT__SWITCH, params))) {
+                          canSwitchProject = true;
+                        }
+                      }
+                    } else {
+                      if (baseAction.hasPermission(baseAction.generatePermission(Permission.PROJECT__SWITCH, params))) {
+                        canSwitchProject = true;
+                      }
+                    }
+
+
+                  } else {
+                    if (baseAction.hasPermission(baseAction.generatePermission(Permission.PROJECT__SWITCH, params))) {
+                      canSwitchProject = true;
+                    }
+                  }
+                }
+              }
+            } else {
+              throw new NullPointerException();
+            }
+          }
+        }
+      }
+    } else {
+      GlobalUnitProject globalUnitProject =
+        globalUnitProjectManager.findByProjectAndGlobalUnitId(project.getId(), loggedCrp.getId());
+      if (globalUnitProject != null) {
+
+        if (project != null && project.isActive()) {
+          if (!globalUnitProject.isOrigin()) {
+            GlobalUnitProject globalUnitProjectOrigin = globalUnitProjectManager.findByProjectId(project.getId());
+            List<Phase> phases =
+              globalUnitProjectOrigin.getGlobalUnit().getPhases().stream()
+                .filter(c -> c.isActive() && c.getDescription().equals(baseAction.getActualPhase().getDescription())
+                  && c.getYear() == baseAction.getActualPhase().getYear() && c.getUpkeep())
+                .collect(Collectors.toList());
+            if (phases.size() > 0) {
+              baseAction.setPhaseID(phases.get(0).getId());
+              project.getProjecInfoPhase(phases.get(0));
+            }
+          }
+          String params[] =
+            {crp.getAcronym(), project.getId() + "", baseAction.getActionName().replaceAll(crp.getAcronym() + "/", "")};
+
+          if (baseAction.canAccessSuperAdmin() || baseAction.canEditCrpAdmin()) {
+            isAdmin = true;
+            if (!baseAction.isSubmit(projectId)) {
+              canSwitchProject = true;
+            }
+            canEdit = true;
+          } else {
+            if (baseAction.hasPermission(baseAction.generatePermission(Permission.PROJECT__PERMISSION, params))) {
+              canEdit = true;
+
+            }
+            if (project.getProjecInfoPhase(baseAction.getActualPhase()) != null) {
+
+              if (!project.getProjecInfoPhase(baseAction.getActualPhase()).isProjectEditLeader()
+                && !baseAction.hasPermission(baseAction.generatePermission(Permission.PROJECT__SWITCH, params))) {
+                canEdit = false;
+              }
+            }
+
+
+            LiaisonUser lUser = liaisonUserManager.getLiaisonUserByUserId(user.getId(), loggedCrp.getId());
+            if (contactPointEditProject && lUser != null) {
+              LiaisonInstitution liaisonInstitution = lUser.getLiaisonInstitution();
+              ProjectPartner projectPartner = project.getLeader();
+
+              if (projectPartner != null) {
+                Institution institutionProject = projectPartner.getInstitution();
+
+                Institution institutionCp = liaisonInstitution.getInstitution();
+
+                if (institutionCp != null) {
+                  if (institutionCp.getId().equals(institutionProject.getId())) {
+                    canSwitchProject = true;
+                  } else {
+                    if (baseAction.hasPermission(baseAction.generatePermission(Permission.PROJECT__SWITCH, params))) {
+                      canSwitchProject = true;
+                    }
+                  }
+                } else {
+                  if (baseAction.hasPermission(baseAction.generatePermission(Permission.PROJECT__SWITCH, params))) {
+                    canSwitchProject = true;
+                  }
+                }
+              } else {
+                if (baseAction.hasPermission(baseAction.generatePermission(Permission.PROJECT__SWITCH, params))) {
+                  canSwitchProject = true;
+                }
+              }
+
+
+            } else {
+              if (baseAction.hasPermission(baseAction.generatePermission(Permission.PROJECT__SWITCH, params))) {
+                canSwitchProject = true;
+              }
+            }
+
+
+            if (baseAction.isSubmit(projectId) && !baseAction.getActualPhase().getUpkeep()) {
+              canEdit = false;
+
+            }
+
+
+            if (baseAction.isCrpClosed()) {
+              if (!(baseAction.hasSpecificities(APConstants.CRP_PMU) && baseAction.isPMU())) {
+                canEdit = false;
+              }
+            }
+
+
+          }
+          if (project.getProjecInfoPhase(baseAction.getActualPhase()).getEndDate() != null) {
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(project.getProjecInfoPhase(baseAction.getActualPhase()).getEndDate());
+            if (project.getProjecInfoPhase(baseAction.getActualPhase()).getStatus().longValue() == Long
+              .parseLong(ProjectStatusEnum.Ongoing.getStatusId())
+              && baseAction.getActualPhase().getYear() > cal.get(Calendar.YEAR)) {
+              canEdit = false;
+              canSwitchProject = false;
+              baseAction.setEditStatus(true);
+
+            }
+
+          }
+
+          if (project.getProjecInfoPhase(baseAction.getActualPhase()).getEndDate() != null) {
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(project.getProjecInfoPhase(baseAction.getActualPhase()).getEndDate());
+            System.out.println(cal.get(Calendar.YEAR));
+            System.out.println(baseAction.getActualPhase().getYear());
+            if (project.getProjecInfoPhase(baseAction.getActualPhase()).getStatus().longValue() == Long
+              .parseLong(ProjectStatusEnum.Complete.getStatusId())) {
+              if (baseAction.getActualPhase().getYear() > cal.get(Calendar.YEAR)) {
+                if (!isAdmin) {
+                  canEdit = false;
+                }
+                canSwitchProject = false;
+              }
+            }
+          }
+
+
+          if (project.getProjecInfoPhase(baseAction.getActualPhase()).getPhase().getDescription()
+            .equals(APConstants.REPORTING)
+            && project.getProjecInfoPhase(baseAction.getActualPhase()).getPhase().getYear() == 2016) {
+            canEdit = false;
+            baseAction.setEditStatus(false);
+          }
+          String actionName = baseAction.getActionName().replaceAll(crp.getAcronym() + "/", "");
+
+          // check permission to edit budget execution in reporting
+          if (baseAction.isReportingActive() && actionName.contains(ProjectSectionStatusEnum.BUDGET.getStatus())) {
+            canEdit = baseAction.canEditAnyProjectExecution(projectId);
+          }
+          // TODO Validate is the project is new
+          if (parameters.get(APConstants.EDITABLE_REQUEST).isDefined()) {
+            String stringEditable = parameters.get(APConstants.EDITABLE_REQUEST).getMultipleValues()[0];
+            editParameter = stringEditable.equals("true");
+            if (!editParameter) {
+              baseAction.setEditableParameter(hasPermissionToEdit);
+            }
+          }
+          // Check the permission if user want to edit or save the form
+          if (editParameter || parameters.get("save").isDefined()) {
+            hasPermissionToEdit = ((baseAction.canAccessSuperAdmin() || baseAction.canEditCrpAdmin())) ? true
+              : baseAction.hasPermission(baseAction.generatePermission(Permission.PROJECT__PERMISSION, params));
+          }
+          if (parameters.get(APConstants.TRANSACTION_ID).isDefined()) {
+            // String stringEditable = ((String[]) parameters.get(APConstants.EDITABLE_REQUEST))[0];
+            editParameter = false;
+          }
+          // If the user is not asking for edition privileges we don't need to validate them.
+          if (!baseAction.getActualPhase().getEditable()) {
+            canEdit = false;
+            baseAction.setCanEditPhase(false);
+          }
+
+          // Check if is a Shared project (Crp to Center)
+          if (!globalUnitProject.isOrigin()) {
+            canEdit = false;
+            if (actionName.equals(SharedProjectSectionStatusEnum.CENTER_MAPPING.getStatus())) {
+              if (baseAction
+                .hasPermission(baseAction.generatePermission(Permission.SHARED_PROJECT_PERMISSION, params))) {
+                canEdit = true;
+              }
+            }
+
+          }
+
+          if (!editParameter) {
+            baseAction.setEditStatus(false);
+          }
+          // Set the variable that indicates if the user can edit the section
+          baseAction.setEditableParameter(editParameter && canEdit && baseAction.getActualPhase().getEditable());
+          baseAction.setCanSwitchProject(canSwitchProject && globalUnitProject.isOrigin());
+          baseAction.setCanEdit(canEdit);
+          baseAction.setEditStatus(baseAction.isEditStatus() && globalUnitProject.isOrigin());
+
+          // Allow Superadmin edit
+          if (baseAction.canAccessSuperAdmin() && editParameter) {
+            baseAction.setEditableParameter(true);
+            baseAction.setCanEdit(true);
+            baseAction.setEditStatus(true);
+          }
+
+
+        } else {
+          throw new NullPointerException();
+        }
+      } else {
+        throw new NullPointerException();
+      }
+    }
+  }
+}
---
+++ marlo-web/src/main/webapp/WEB-INF/crp/views/projects/projectsCenterList.ftl
@@ -0,0 +1,96 @@
+[#ftl]
+[#assign title = "MARLO Projects" /]
+[#assign currentSectionString = "${actionName?replace('/','-')}-phase-${(actualPhase.id)!}" /]
+[#assign pageLibs = ["datatables.net", "datatables.net-bs","font-awesome"] /]
+[#assign customJS = [
+  "https://cdn.datatables.net/buttons/1.3.1/js/dataTables.buttons.min.js",
+  "//cdn.datatables.net/buttons/1.3.1/js/buttons.html5.min.js",
+  "//cdn.datatables.net/buttons/1.3.1/js/buttons.print.min.js",
+  "${baseUrlMedia}/js/projects/projectsList.js" 
+  ] 
+/]
+[#assign customCSS = [
+  "${baseUrl}/global/css/customDataTable.css", 
+  "${baseUrlMedia}/css/projects/projectsList.css"] 
+/]
+
+[#assign currentSection = "projects" /]
+[#assign currentStage = (filterBy)!"all" /]
+
+
+[#assign breadCrumb = [
+  {"label":"projectsList", "nameSpace":"/projects", "action":""}
+]/]
+
+[#include "/WEB-INF/global/pages/header.ftl" /]
+[#include "/WEB-INF/global/pages/main-menu.ftl" /]
+[#import "/WEB-INF/crp/macros/projectsListTemplate.ftl" as projectList /]
+
+<div class="container helpText viewMore-block">
+  <div class="helpMessage infoText">
+    <img class="col-md-2" src="${baseUrl}/global/images/icon-help.jpg" />
+    <p class="col-md-10"> [@s.text name="projectsList.help"][@s.param]${currentCycle}[/@s.param][/@s.text] </p>
+  </div> 
+  <div style="display:none" class="viewMore closed"></div>
+</div>
+
+<section class="container">
+  <article class="row" id="mainInformation">
+    <div class="col-md-12">
+      <div class="loadingBlock"></div>
+      <div style="display:none" >
+        <!-- Nav tabs -->
+        <ul class="nav nav-tabs" role="tablist">
+          <li role="presentation" class="active">
+            <a href="#active-tab" aria-controls="home" role="tab" data-toggle="tab">
+              <strong><span class="glyphicon glyphicon-flag"></span>  [@s.text name="projectsList.active"/] </strong> <br /><small>([@s.text name="projectsList.active${reportingActive?string('Reporting', 'Planning')}.help"/])</small>
+            </a>
+          </li>
+          [#if !reportingActive]
+          <li role="presentation">
+            <a href="#archived-tab" aria-controls="profile" role="tab" data-toggle="tab">
+              <strong><span class="glyphicon glyphicon-inbox"></span> [@s.text name="projectsList.archived"/] </strong> <br /><small>([@s.text name="projectsList.archived.help"/])</small>
+            </a>
+          </li>
+          [/#if]
+        </ul>
+      
+        <!-- Tab panes -->
+        <div class="tab-content">
+          <div role="tabpanel" class="tab-pane active" id="active-tab">
+            [#-- Projects List (My Projects) --]
+            <h3 class="headTitle text-center">[@s.text name="projectsList.yourProjects"/]</h3>
+            [@projectList.projectsList projects=myProjects canValidate=true canEdit=true namespace="/projects" defaultAction="${(crpSession)!}/description" /]
+            <hr/>
+            [#-- Projects List (Other Projects) --]
+            <h3 class="headTitle text-center">[@s.text name="projectsList.otherProjects" /] <br /> <small>[@s.text name="projectsList.otherProjects.help" /]</small></h3>
+            [@projectList.projectsList projects=allProjects canValidate=true namespace="/projects" defaultAction="${(crpSession)!}/description"/]
+          </div>
+          [#if !reportingActive]
+          <div role="tabpanel" class="tab-pane" id="archived-tab">
+            [#-- Archived Projects List (My Projects) --]
+            <h3 class="headTitle text-center">[@s.text name="projectsList.archivedProjects"/]</h3>
+            [@projectList.projectsListArchived projects=(closedProjects)! canValidate=false canEdit=false namespace="/projects" defaultAction="${(crpSession)!}/description" /]
+          </div>
+          [/#if]
+        </div>
+      </div>
+      [#-- Section Buttons --]
+      [#if (action.canAddCoreProject() || action.canAddBilateralProject()) && (!crpClosed) && !reportingActive && action.getActualPhase().editable]
+      <div class="buttons">
+        <div class="buttons-content">
+          <a class="addButton" href="[@s.url action='${crpSession}/addNewCoreProject'][#include "/WEB-INF/global/pages/urlGlobalParams.ftl" /][/@s.url]">[@s.text name="projectsList.addResearchProject" /]</a>
+          <a class="addButton" href="[@s.url action='${crpSession}/addNewAdminProject'][#include "/WEB-INF/global/pages/urlGlobalParams.ftl" /][/@s.url]">[@s.text name="projectsList.addManagementProject" /]</a>
+          <div class="clearfix"></div>
+        </div>
+      </div>
+      <div class="clearfix"></div>
+      [/#if]
+    </div>
+    
+  </article>
+</section>
+[@customForm.confirmJustification action="deleteProject.do" namespace="/${currentSection}" title="Remove Project" /]
+
+
+[#include "/WEB-INF/global/pages/footer.ftl"]
